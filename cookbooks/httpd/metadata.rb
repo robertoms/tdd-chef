5b7e70ee008c29a93597cfb0efe2fb8acf1b988d
pm <pm>
2017-06-21 18:41:50 +0200

    segundo commit

--- cookbooks/httpd/metadata.rb
+++ cookbooks/httpd/metadata.rb
@@ -8,8 +8,8 @@ version '0.1.0'
 
 # If you upload to Supermarket you should set this so your cookbook
 # gets a `View Issues` link
-# issues_url 'https://github.com/<insert_org_here>/httpd/issues' if respond_to?(:issues_url)
+ issues_url 'https://github.com/robertoms/tdd-chef/issues'
 
 # If you upload to Supermarket you should set this so your cookbook
 # gets a `View Source` link
-# source_url 'https://github.com/<insert_org_here>/httpd' if respond_to?(:source_url)
+ source_url 'https://github.com/robertoms/tdd-chef.git'
