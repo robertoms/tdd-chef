6790898fc49e995cf99117132e594c40659ae935
pm <pm>
2017-06-21 18:32:19 +0200

    Mi primer commit

---
+++ cookbooks/httpd/README.md
@@ -0,0 +1,5 @@
+# httpd
+
+Cookbook elaborado a partir de test
+  - Servidor Apache escuchando el puerto 80
+  - Página que devuelve: Hello World!
---
+++ cookbooks/httpd/metadata.rb
@@ -0,0 +1,15 @@
+name 'httpd'
+maintainer 'Roberto'
+maintainer_email 'yo@yoyo.com'
+license 'all_rights'
+description 'Installs/Configures httpd'
+long_description 'Installs/Configures httpd'
+version '0.1.0'
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Issues` link
+# issues_url 'https://github.com/<insert_org_here>/httpd/issues' if respond_to?(:issues_url)
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Source` link
+# source_url 'https://github.com/<insert_org_here>/httpd' if respond_to?(:source_url)
